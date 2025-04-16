35d2a753f114da1b713281ea75422609fe636ac7
aman5684 <128953355+aman5684@users.noreply.github.com>
2025-04-16 10:57:06 +0545

    Add files via upload

---
+++ week 1/src/Weekly_assignment/PersonalDetails.java
@@ -0,0 +1,10 @@
+package Weekly_assignment;
+
+public class PersonalDetails {
+    public static void main(String[] args) {
+        System.out.println("Name: aman kumar");
+        System.out.println("Address: Kathmandu, Nepal");
+        System.out.println("Age: 22");
+        System.out.println("Phone Number: 988888888");
+    }
+}
